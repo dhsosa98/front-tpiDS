5bfffbd494c69b4a5320f8a061c2739981d97be9
Diego Sosa <dhsosa98@gmail.com>
2021-12-20 13:15:52 -0300

    fixed app port

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 8080
+
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(port, () => {
-    console.log('Example app listening')
-  })
+app.listen(process.env.PORT || 8080);
